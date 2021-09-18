fddd8796f535327fe144c274e58a4979f26e554f
JeonGeunHee <weiding11086@gmail.com>
2021-09-18 22:17:42 +0900

    merger Sort Success
    Asce Sort Success

--- Algorithm-DefaultSort/MergeSort.cpp
+++ Algorithm-DefaultSort/MergeSort.cpp
@@ -6,9 +6,61 @@ using namespace std;
 //크게 두 역할로 나눌꺼임.
 //배열을 재귀로 통해 쪼개는 부분
 //나눈 부분을 비교하여 정렬기준대로 정렬한 뒤 삽입 할 부분?
+//더 이상 쪼갤수 없을때 까지 쪼갠 뒤
+//하나씩 합치면서 정렬을 해서 합치는 방법.
 
-void MergeSort(int _iArr[], int _iSize)
-{
+#pragma region ReferenceSolution
+void Merge(int _iArr[], int _iLeft, int _iCenter, int _iRight)
+{//여기서 쪼갠 원소들을 합치면서 정렬 해줄 부분.
+
+	int i = _iLeft;
+	int j = _iCenter + 1;
+	int k = _iLeft;
+
+	int resultArr[11];
+
+	while (i <= _iCenter && j <= _iRight)
+	{//두 배열중 하나가 끝날때까지 합침.
+		if (_iArr[i] <= _iArr[j])
+		{//전위,후위 연산 잘 생각해서 써야함.
+			//전위를 사용해 버리면 숫자를 키우고 나서 대입을 하는거임으로
+			//맨처음 기준으로 resultArr[1] = iArr[1];을 하는건데
+			//후위 연산으로 들어가면
+			//resultArr[0] = iArr[0]을 하고나서 증가연산을 하기 때문에
+			//다른 값이 나올 수 밖에 없음.
+			//resultArr[++k] = _iArr[++i];
+			resultArr[k++] = _iArr[i++];
+		}
+		else
+		{
+			resultArr[k++] = _iArr[j++];
+		}
+	}
+
+	if (i > _iCenter)
+	{//윗 과정이 끝나고 오른쪽 배열에 남은 원소가 있다면 넣기
+		for (int x = j; x <= _iRight; ++x)
+		{
+			resultArr[k++] = _iArr[x];
+		}
+	}
+	else 
+	{//윗 과정이 끝나고 왼쪽 배열에 남은 원소가 있다면 넣기
+		for (int x = i; x <= _iCenter; ++x)
+		{
+			resultArr[k++] = _iArr[x];
+		}
+	}
+
+	for (int x = _iLeft; x <= _iRight; ++x)
+	{//실제 배열로 다시 옮기기
+		_iArr[x] = resultArr[x];
+	}
+}
+#pragma endregion
+
+void MergeSort(int _iArr[], int _iLeft, int _iRight)
+{//여기가 재귀 돌리면서 배열을 쪼개는 부분.
 
 #pragma region MySolution-Wrong
 	//int iHalfSize1 = _iSize / 2;
@@ -69,34 +121,25 @@ void MergeSort(int _iArr[], int _iSize)
 
 	////이 두덩이들을 하나씩 비교하면서 배열에 넣자
 #pragma endregion
-	
-#pragma region ReferenceSolution
 
+#pragma region ReferenceSolution 
+	int iCenterIndex;
 
+	if (_iLeft < _iRight)
+	{
+		iCenterIndex = (_iLeft + _iRight) / 2;
 
+		//여기서 왼쪽 부분을 더 쪼갤 수 없을 때까지 재귀 돌리는거
+		MergeSort(_iArr, _iLeft, iCenterIndex);
 
+		//위에서 왼쪽을 쭉 쪼갰으면 오른쪽도 더 쪼갤 수 없을 떄 까지 쪼갬.
+		MergeSort(_iArr, iCenterIndex + 1, _iRight);
 
+		Merge(_iArr, _iLeft, iCenterIndex, _iRight);
 
+	}
 
 #pragma endregion
-
-
-
-
-
-	//for (int i = 0; i < iHalfSize; ++i)
-	//{
-	//	cout << iArr1[i] << endl;
-	//}
-
-	//cout << "--------" << endl;
-
-	//for (int i = 0; i < _iSize - (iHalfSize); ++i)
-	//{
-	//	cout << iArr2[i] << endl;
-	//}
-
-
 };
 
 
@@ -120,7 +163,12 @@ void main()
 
 	int iArrSize = sizeof(iArr) / sizeof(int);
 
-	MergeSort(iArr, iArrSize);
+	MergeSort(iArr, 0, iArrSize-1);
+
+	for (int i = 0; i < iArrSize; ++i)
+	{
+		cout << iArr[i] << endl;
+	}
 
 	system("pause");
 
